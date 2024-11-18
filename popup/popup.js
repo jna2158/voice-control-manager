492af0a614521c837d4b70055048e8a7211a8c77
유지원 <jna215843@gmail.com>
2024-11-18 12:52:09 +0900

    refactor: 권한 체크 함수로 분리

--- popup/popup.js
+++ popup/popup.js
@@ -1,33 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const voiceButton = document.getElementById("voiceButton");
   const statusText = document.getElementById("status");
-  let isRecognizing = false;
+  let isRecognizing = false; // 음성인식 실행중인지
 
-  if (!("webkitSpeechRecognition" in window)) {
+  const isRecog = checkVoiceRecognition();
+  if (!isRecog) {
     voiceButton.textContent = "이 브라우저는 음성 인식을 지원하지 않습니다.";
     voiceButton.disabled = true;
-    return;
   }
 
   const recognition = new webkitSpeechRecognition();
   recognition.lang = "ko-KR";
   recognition.continuous = true;
-  recognition.interimResults = false;
-
-  checkIsDenied();
-  recognition.start();
-
-
-  voiceButton.addEventListener("click", async () => {
-    if (isRecognizing) {
-      recognition.stop();
-      return;
-    }
-
-    await checkIsDenied();
-    recognition.start();
-  });
-
+  recognition.interimResults = true;
+  
   recognition.onstart = () => {
     console.log("음성인식이 시작됨");
     isRecognizing = true;
@@ -53,41 +39,62 @@ document.addEventListener("DOMContentLoaded", () => {
     isRecognizing = false;
     voiceButton.textContent = "음성 인식 시작";
   };
+
+  // voiceButton 클릭했을 때
+  voiceButton.addEventListener("click", async () => {
+    if (isRecognizing) {
+      recognition.stop();
+      return;
+    }
+
+    const permission = await checkMicPermission();
+    await allowMicPermission(permission);
+    recognition.start();
+  });
 });
 
+// 해당 브라우저 음성 인식 지원하는지 확인
+const checkVoiceRecognition = () => {
+  if (("webkitSpeechRecognition" in window)) {
+    return true;
+  } return false;
+};
+
 // 마이크 권한 허용 여부 확인
-const checkIsDenied = async () => {
+const checkMicPermission = async () => {
   try {
     const permissionStatus = await navigator.permissions.query({ name: "microphone" });
-    
-    // 권한이 거부되었을 때
-    if (permissionStatus.state === "denied") {
-      isMicDenied();
-    }
-
-    // 권한이 없는 경우
-    if (permissionStatus.state === "prompt") {
-      const stream = await navigator.mediaDevices.getUserMedia({audio: true});
-      stream.getTracks().forEach(track => track.stop());
-    }
+    return permissionStatus.state;
   } catch (err) {
-    if (err.name === "NotAllowedError") {
-      isMicDenied();
-    } else {
-      statusText.textContent = `오류가 발생했습니다: ${err.message}`;
-    }
+    return err;
+  }
+};
+
+// 마이크 권한 허용
+const allowMicPermission = async (permission) => {
+  // 권한이 거부되었을 때
+  if (permission === "denied") {
+    goToMicSetting();
+  }
+
+  // 권한이 없는 경우
+  if (permission === "prompt") {
+    const stream = await navigator.mediaDevices.getUserMedia({audio: true});
+    stream.getTracks().forEach(track => track.stop());
   }
 };
 
 // 마이크 권한 거부상태일 때 설정으로 이동
-const isMicDenied = () => {
+const goToMicSetting = () => {
+  const statusText = document.getElementById("status");
   statusText.textContent = "마이크 권한이 거부되었습니다. 설정으로 이동하시겠습니까?";
-  const settingsLink = document.createElement("button");
-  settingsLink.textContent = "마이크 설정으로 이동";
+  const settingsLink = document.createElement("a");
+  settingsLink.className = "settings-link";
   settingsLink.onclick = () => {
     chrome.tabs.create({
       url: "chrome://settings/content/microphone"
     });
   };
+  settingsLink.textContent = "마이크 설정으로 이동 ->";
   statusText.appendChild(settingsLink);
 };
